9f5af52b1d77c4d3ad4fdaca667725bcbd3fa2e8
ChoiHyoSeok <36808015+supremehyo@users.noreply.github.com>
2024-08-18 20:45:47 +0900

    add : 디자인시스템 textButton 컴포넌트 새로 생성

--- core/designsystem/src/main/java/com/hyoseok/samplecleanarchitecture/core/designsystem/component/TextButtonComponent.kt
+++ core/designsystem/src/main/java/com/hyoseok/samplecleanarchitecture/core/designsystem/component/TextButtonComponent.kt
@@ -1,30 +1,50 @@
 package com.hyoseok.samplecleanarchitecture.core.designsystem.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hyoseok.samplecleanarchitecture.core.designsystem.theme.LocalTypography
 
 @Composable
 fun TextButtonComponent(
-    name : String,
-    backColor : Int,
-    onClick : () -> Unit
+    name: String,
+    backColor: Color,
+    textColor: Color,
+    onClick: () -> Unit
 ) {
     Box(
-        modifier = Modifier.clip(RoundedCornerShape(8.dp))
-    ){
-        Text(text = "재촬영" , style = LocalTypography.current.displaySmallR)
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(backColor)
+            .clickable {
+                onClick.invoke()
+            }
+    ) {
+        Text(
+            text = name,
+            style = LocalTypography.current.displaySmallR.copy(textColor),
+            modifier = Modifier.padding(4.dp)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomePreview() {
+    TextButtonComponent(
+        name = "테스트",
+        backColor = Color.Black,
+        textColor = Color.White
+    ) {
 
+    }
 }
