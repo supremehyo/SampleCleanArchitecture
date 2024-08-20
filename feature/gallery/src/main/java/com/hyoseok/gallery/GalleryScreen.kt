d6c222cedfab39a024be554bbf5386adaa24e4e9
ChoiHyoSeok <36808015+supremehyo@users.noreply.github.com>
2024-08-20 21:28:45 +0900

    feature : 다중 선택한 이미지를 하단 리스트로 구현하여 선택시 편집 이미지 변경 기능 구현

--- feature/gallery/src/main/java/com/hyoseok/gallery/GalleryScreen.kt
+++ feature/gallery/src/main/java/com/hyoseok/gallery/GalleryScreen.kt
@@ -4,15 +4,36 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.hyoseok.samplecleanarchitecture.core.designsystem.theme.LocalTypography
 
 @Composable
 fun GalleryImageScreen() {
+    var selectedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
     var selectedImageUris by remember {
         mutableStateOf<List<Uri>>(emptyList())
     }
@@ -21,6 +42,9 @@ fun GalleryImageScreen() {
         contract = ActivityResultContracts.PickMultipleVisualMedia(),
         onResult = { uris ->
             selectedImageUris = uris
+            if (selectedImageUris.isNotEmpty()) {
+                selectedImageUri = selectedImageUris.first()
+            }
         }
     )
 
@@ -32,5 +56,42 @@ fun GalleryImageScreen() {
         )
     }
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(text = "사진 편집", style = LocalTypography.current.headlineMediumB)
+        AsyncImage(
+            model = selectedImageUri,
+            contentDescription = null,
+            modifier = Modifier.size(400.dp),
+            contentScale = ContentScale.Crop
+        )
+        Column {
+            Text(
+                text = "선택하여 편집 이미지를 변경 할 수 있습니다.",
+                modifier = Modifier.padding(start = 8.dp, bottom = 2.dp)
+            )
+            LazyColumn(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                items(selectedImageUris) { uri ->
+                    AsyncImage(
+                        model = uri,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clickable {
+                                selectedImageUri = uri
+                            },
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
 
+        }
+    }
 }
